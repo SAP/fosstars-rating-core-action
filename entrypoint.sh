3fd753282b0f36dfd566c997eaf82ee5fbea3701
Artem Smotrakov <artem.smotrakov@sap.com>
2021-02-09 17:31:34 +0100

    Added parameters for filenames
    Part of #2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
 REPORT_BRANCH=$1
-TOKEN=$2
+REPORT_FILE=$2
+BADGE_FILE=$3
+TOKEN=$4
+
+if [ "$REPORT_BRANCH" = "" ]; then
+    echo "Oops! No branch provided!"
+    exit 1
+fi
+
+if [ "$REPORT_FILE" = "" ]; then
+    REPORT_FILE="fosstars_report.md"
+fi
+
+if [ "$BADGE_FILE" = "" ]; then
+    BADGE_FILE="fosstars_badge.svg"
+fi
 
 PROJECT_SCM_URL=$GITHUB_SERVER_URL/$GITHUB_REPOSITORY
+RAW_RATING_FILE="fosstars_security_rating.json"
 
 # Switch to the branch where the report should be stored
 git fetch origin $REPORT_BRANCH || git branch $REPORT_BRANCH
@@ -15,24 +31,22 @@ if [ $? -ne 0 ]; then
 fi
 
 # Generate a report
-report_file="fosstars_security_rating.md"
-raw_rating_file="fosstars_security_rating.json"
 java -jar /opt/stuff/fosstars-rating-core/target/fosstars-github-rating-calc.jar \
           --url $PROJECT_SCM_URL \
           --token $TOKEN \
           --verbose \
-          --report-file $report_file \
+          --report-file $REPORT_FILE \
           --report-type markdown \
-          --raw-rating-file $raw_rating_file
+          --raw-rating-file $RAW_RATING_FILE
 
-git add $report_file $raw_rating_file
-if git status | grep "nothing to commit" > /dev/null ; then
+git add $REPORT_FILE $RAW_RATING_FILE
+if git status | grep "nothing to commit" > /dev/null 2>&1 ; then
     echo "No updates found"
     exit 0
 fi
 
 # Update the current badge
-label=$(cat $raw_rating_file | jq -r .label[1] | tr '[:upper:]' '[:lower:]' | sed 's/ //g')
+label=$(cat $RAW_RATING_FILE | jq -r .label[1] | tr '[:upper:]' '[:lower:]' | sed 's/ //g')
 case $label in
     good|moderate|bad|unclear)
       suffix=$label
@@ -41,14 +55,13 @@ case $label in
       suffix="unknown"
       ;;
 esac
-current_badge_file="fosstars-security-rating.svg"
-wget -O $current_badge_file https://raw.githubusercontent.com/SAP/fosstars-rating-core/master/.github/actions/fosstars-create-single-report/images/security-fosstars-$suffix.svg
-git add $current_badge_file
+wget -O $BADGE_FILE https://raw.githubusercontent.com/SAP/fosstars-rating-core/master/.github/actions/fosstars-create-single-report/images/security-fosstars-$suffix.svg
+git add $BADGE_FILE
 
 # Commit the report and the badge
 set -e
 git config --global user.name "Fosstars"
 git config --global user.email "fosstars@users.noreply.github.com"
-git commit -m "Update Fosstars security rating report" $report_file $current_badge_file $raw_rating_file
+git commit -m "Update Fosstars security rating report" $REPORT_FILE $BADGE_FILE $RAW_RATING_FILE
 git remote set-url origin https://x-access-token:$TOKEN@github.com/$GITHUB_REPOSITORY
 git push origin $REPORT_BRANCH
