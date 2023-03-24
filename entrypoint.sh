b7412f6ded2f721526f1323b7dfbc39e3514e488
Florian Vogt <florian.vogt@sap.com>
2023-03-24 08:41:45 +0100

    Quote user name and email to allow whitespaces

--- entrypoint.sh
+++ entrypoint.sh
@@ -134,8 +134,8 @@ wget -O $BADGE_FILE https://raw.githubusercontent.com/SAP/fosstars-rating-core-a
 git add $BADGE_FILE
 
 # Commit the report and the badge
-git config --global user.name $GIT_USER_NAME
-git config --global user.email $GIT_USER_EMAIL
+git config --global user.name "$GIT_USER_NAME"
+git config --global user.email "$GIT_USER_EMAIL"
 
 git commit -m "Update Fosstars report" $REPORT_FILE $BADGE_FILE $RAW_RATING_FILE
 if [ $? -ne 0 ]; then
